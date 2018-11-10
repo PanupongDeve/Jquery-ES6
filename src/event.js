e3091ed24301a433d5fc685a1943911b560294c6
PanupongDev <5635512110@psu.ac.th>
2018-11-11 01:28:47 +0700

    updatte mouse event

--- src/event.js
+++ src/event.js
@@ -16,6 +16,9 @@ export class JqueryEvent {
     mouseEventStart() {
         this.countNumber.text(this.count);
         this.eventClick();
+        this.eventDoubleClick();
+        this.eventMouseEnter();
+        this.eventMouseLeave();
     }
 
     eventClick() {
@@ -26,5 +29,34 @@ export class JqueryEvent {
             }
             this.countNumber.text(this.count);
         });
+    }
+
+    eventDoubleClick() {
+        this.mouseDoubleClick.on('dblclick', () => {
+            this.countNumber.text('Game Over You Doble Click.');
+            setTimeout(() => {
+                this.countNumber.hide(); 
+            }, 2000);
+        });   
+    }
+
+    eventMouseEnter() {
+        this.mouseEnter.on('mouseenter', () => {
+            this.countNumber.text('Game Over You Mouse Enter.');
+            setTimeout(() => {
+                this.countNumber.hide(); 
+            }, 2000);
+        })
+    }
+
+    eventMouseLeave() {
+        this.mouseEnter.on('mouseleave', () => {
+            this.countNumber.text('Game Restart.');
+            this.count = 0;
+            setTimeout(() => {
+                this.countNumber.show();
+                this.countNumber.text(this.count);
+            }, 2000);
+        })
     }
 }
